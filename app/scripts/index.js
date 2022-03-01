fce282cce9aa2b01d6f28d07ea0837bbb1ad6964
Alinawaz-786 <alinawaz1857@gmail.com>
2022-03-01 23:14:30 +0500

    v1 (1-03-2022)

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,10 +7,18 @@ import Web3 from 'web3'
 import contract from 'truffle-contract'
 
 import CurrencyCoinArtifact from '../../build/gsn/Currency.json'
+import FactoryCoinArtifact from '../../build/gsn/Factory.json'
+import CapitalReserveArtifact from '../../build/gsn/CapitalReserve.json'
+import OFERC20Artifact from '../../build/gsn/OFERC20.json'
+
 import { networks } from './networks'
 
 
 const CurrencyCoin = contract(CurrencyCoinArtifact)
+const FactorySmartContract = contract(FactoryCoinArtifact)
+const CapitalReserveContract = contract(CapitalReserveArtifact)
+const OFERC20Contract = contract(OFERC20Artifact)
+
 
 
 let accounts
@@ -74,7 +82,6 @@ const App = {
         accounts = accs
         account = accounts[0]
 
-        self.refreshBalance()
       })
     })
   },
@@ -84,71 +91,28 @@ const App = {
     status.innerHTML = message
   },
 
-  link: function (path, text) {
-    return '<a href="' + network.baseurl + path + '">' + text + '</a>'
-  },
-
-  addressLink: function (addr) {
-    return '<a href="' + network.addressUrl + addr + '" target="_info">' + addr + '</a>'
-  },
-
-  txLink: function (addr) {
-    return '<a href="' + network.txUrl + addr + '" target="_info">' + addr + '</a>'
-  },
-
-  refreshBalance: function () {
+  //Currency Token
+  currency_mint: function () {
     const self = this
-
-   
-    const CurrentAccountElement = document.getElementById('address')
-    CurrentAccountElement.innerHTML = account
-
-
-   
-
+    const currency_mint = parseInt(document.getElementById('currency_mint').value)
+    this.setStatus('Initiating transaction... (please wait)')
     let meta
     CurrencyCoin.deployed().then(function (instance) {
       meta = instance
-      putAddr('address', account)
-      putAddr('metaaddr', CurrencyCoin.address)
-
-      return meta.balanceOf.call(account, { from: account })
-    }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
-
-    }).catch(function (e) {
-      const fatalmessage = document.getElementById('fatalmessage')
-      console.log(e)
-      if (/mismatch/.test(e)) {
-        fatalmessage.innerHTML = "Wrong network. please switch to 'kovan', 'rinekby', or 'ropsten' "
-      }
-      self.setStatus('Error getting balance; see log.')
-    })
-  },
-
-  mint: function () {
-    const self = this
-    CurrencyCoin.deployed().then(function (instance) {
-      self.setStatus('Mint: Initiating transaction... (please wait)')
-      return instance.mint({ from: account })
+      return meta.mint(currency_mint,
+        { from: account })
     }).then(function (res) {
-      self.refreshBalance()
-      self.setStatus('Mint transaction complete!<br>\n' + self.txLink(res.tx))
-    }).catch(function (err) {
-      console.log('mint error:', err)
-      self.setStatus('Error getting balance; see log.')
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
     })
   },
-
-  transfer: function () {
+  currency_transfer: function () {
     const self = this
-
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-
+    const amount = parseInt(document.getElementById('currency_amount').value)
+    const receiver = document.getElementById('currency_receiver').value
     this.setStatus('Initiating transaction... (please wait)')
-
     let meta
     CurrencyCoin.deployed().then(function (instance) {
       meta = instance
@@ -156,12 +120,222 @@ const App = {
         { from: account })
     }).then(function (res) {
       self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
-      self.refreshBalance()
     }).catch(function (e) {
       console.log(e)
       self.setStatus('Error sending coin; see log.')
     })
-  }
+  },
+  currency_approve: function(){
+    alert("approve")
+    const self = this
+    const currency_spender_amount = parseInt(document.getElementById('currency_spender_amount').value)
+    const currency_spender = document.getElementById('currency_spender').value
+    this.setStatus('Initiating transaction... (please wait)')
+    let meta
+    CurrencyCoin.deployed().then(function (instance) {
+      meta = instance
+      return meta.approve(currency_spender, currency_spender_amount,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  },
+  //Currency End
+
+  //Factory
+
+  factory_deploy_capital_reserve: function(){
+    alert('test')
+    const self = this
+    const f_deployCapitalReserve = document.getElementById('f_deployCapitalReserve').value
+    const f_reserveGov = document.getElementById('f_reserveGov').value
+    const f_regulator = document.getElementById('f_regulator').value
+    const f_token = document.getElementById('f_token').value
+    const f_interestRate = parseInt(document.getElementById('f_interestRate').value)
+    const f_intervalSeconds = parseInt(document.getElementById('f_intervalSeconds').value)
+
+    let meta
+    FactorySmartContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.deployCapitalReserve(f_deployCapitalReserve, 
+        f_reserveGov,
+        f_regulator,
+        f_token,
+        f_interestRate,
+        f_intervalSeconds,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  },
+  factory_setMaxInterestLimite: function(){
+    const f_interestRate = parseInt(document.getElementById('f_interestRate').value)
+    let meta
+    FactorySmartContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.setMaxInterestLimit(f_interestRate,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  },
+  factory_whiteList: function(){
+    const f_whiteList = document.getElementById('f_whiteList').value
+    let meta
+    FactorySmartContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.whitelist(f_whiteList,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  }, 
+  //Factory End
+
+  //Capital Reserve 
+  cr_approve: function(){
+    const cr_spender = document.getElementById('cr_spender').value
+    const cr_amount = parseInt(document.getElementById('cr_amount').value)
+    let meta
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.approve(cr_spender,cr_amount,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  },
+  cr_claim_pending_transection: function(){
+    const cr_claim_pending_transection = document.getElementById('cr_claim_pending_transection').value
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.claimPendingTransaction(cr_claim_pending_transection,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    })
+  },
+  cr_cancel_pending_transection: function(){
+    const cr_cancel_pending_transection = document.getElementById('cr_cancel_pending_transection').value
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.cancelPendingTransaction(cr_cancel_pending_transection,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  btn_cr_deposit_capital_reserve_pool: function(){
+    const cr_deposit_capital_reserve_pool = document.getElementById('cr_deposit_capital_reserve_pool').value
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.depositToCapitalReservePool(cr_deposit_capital_reserve_pool,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  btn_cr_deposit_interest_pool: function(){
+    const cr_deposit_interest_pool = document.getElementById('cr_deposit_interest_pool').value
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.depositToCapitalReservePool(cr_deposit_interest_pool,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  btn_cr_recoverAccidentalTransfer: function(){
+    const cr_accidental_goverance_token = document.getElementById('cr_accidental_goverance_token').value
+    const cr_accidental_goverance_amount = parseInt(document.getElementById('cr_accidental_goverance_amount').value)
+    
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.recoverAccidentalTransfer(
+        cr_accidental_goverance_token,
+        cr_accidental_goverance_amount,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  btn_cr_withdraw_capital_reserve_pool: function(){
+    const cr_withdraw_capital_reserve_pool = parseInt(document.getElementById('cr_withdraw_capital_reserve_pool').value)
+    
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.withdrawFromCapitalReservePool(
+        cr_withdraw_capital_reserve_pool,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  btn_cr_withdraw_interest_pool: function(){
+    const cr_withdraw_interest_pool = parseInt(document.getElementById('cr_withdraw_interest_pool').value)
+    CapitalReserveContract.deployed().then(function (instance) {
+      meta = instance
+      return meta.withdrawFromCapitalReservePool(
+        cr_withdraw_interest_pool,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+  //Capital Reserve End
+  btn_oferc20_token: function(){
+    const oferd20_token_address = parseInt(document.getElementById('oferd20_token_address').value)
+    const oferd20_amount = parseInt(document.getElementById('oferd20_amount').value)
+    OFERC20Contract.deployed().then(function (instance) {
+      meta = instance
+      return meta.withdrawFromCapitalReservePool(
+        oferd20_token_address,
+        oferd20_amount,
+        { from: account })
+    }).then(function (res) {
+      self.setStatus('Transaction complete!<br>\n' + self.txLink(res.tx))
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error sending coin; see log.')
+    }) 
+  },
+
 }
 
 window.App = App
